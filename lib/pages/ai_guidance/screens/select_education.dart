c32ff7358798e7e9594f8eeb6eefff2c6f2cb31f
Anas Nadeem <nadeemanas617@gmail.com>
2024-12-28 19:50:12 +0530

    new loader

--- lib/pages/ai_guidance/screens/select_education.dart
+++ lib/pages/ai_guidance/screens/select_education.dart
@@ -8,53 +8,140 @@ class SelectEducation extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
-        padding: EdgeInsets.all(16),
+        padding: EdgeInsets.all(screenWidth * 0.04),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text(
-              'Choose Your Current Education Level',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            Text(
+              'Please select your current level of education from the options below:',
+              style: Theme.of(context).textTheme.titleLarge,
+              textAlign: TextAlign.center,
             ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'School',
-                  style: TextStyle(fontSize: 16),
+            SizedBox(height: screenHeight * 0.04),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (ctx) => const CareerSurvey()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(screenWidth * 0.4, screenWidth * 0.4),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(screenWidth * 0.04),
                 ),
-                IconButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (ctx) => const CareerSurvey()),
-                    );
-                  },
-                  icon: const Icon(CupertinoIcons.arrow_right),
-                ),
-              ],
+              ),
+              child: Column(
+                children: [
+                  const Icon(
+                    Icons.school,
+                    size: 50,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(height: screenHeight * 0.01),
+                  const Text('School',
+                      style: TextStyle(fontSize: 20, color: Colors.blue)),
+                ],
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Higher Education',
-                  style: TextStyle(fontSize: 16),
+            SizedBox(height: screenHeight * 0.02),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (ctx) => const CareerSurveyHigher()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(screenWidth * 0.4, screenWidth * 0.4),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(screenWidth * 0.04),
                 ),
-                IconButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(builder: (ctx) => const CareerSurveyHigher()),
-                    );
-                  },
-                  icon: const Icon(CupertinoIcons.arrow_right),
+              ),
+              child: Column(
+                children: [
+                  const Icon(Icons.military_tech,
+                      size: 50, color: Colors.green),
+                  SizedBox(height: screenHeight * 0.01),
+                  const Text('Graduation',
+                      style: TextStyle(fontSize: 20, color: Colors.green)),
+                ],
+              ),
+            ),
+            SizedBox(height: screenHeight * 0.02),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (ctx) => const CareerSurveyHigher()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(screenWidth * 0.4, screenWidth * 0.4),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(screenWidth * 0.04),
                 ),
-              ],
+              ),
+              child: Column(
+                children: [
+                  const Icon(
+                    CupertinoIcons.book,
+                    size: 50,
+                    color: Colors.purple,
+                  ),
+                  SizedBox(height: screenHeight * 0.01),
+                  const Text('Post-Graduation',
+                      style: TextStyle(fontSize: 14, color: Colors.purple)),
+                ],
+              ),
             ),
           ],
+          // children: [
+          //   const Text(
+          //     'Choose Your Current Education Level',
+          //     style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          //   ),
+          //   SizedBox(height: 20),
+          //   Row(
+          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //     children: [
+          //       const Text(
+          //         'School',
+          //         style: TextStyle(fontSize: 16),
+          //       ),
+          //       IconButton(
+          // onPressed: () {
+          //   Navigator.of(context).push(
+          //     MaterialPageRoute(builder: (ctx) => const CareerSurvey()),
+          //   );
+          //         },
+          //         icon: const Icon(CupertinoIcons.arrow_right),
+          //       ),
+          //     ],
+          //   ),
+          //   Row(
+          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //     children: [
+          //       const Text(
+          //         'Higher Education',
+          //         style: TextStyle(fontSize: 16),
+          //       ),
+          //       IconButton(
+          //         onPressed: () {
+          //           Navigator.of(context).push(
+          //             MaterialPageRoute(builder: (ctx) => const CareerSurveyHigher()),
+          //           );
+          //         },
+          //         icon: const Icon(CupertinoIcons.arrow_right),
+          //       ),
+          //     ],
+          //   ),
+          // ],
         ),
       ),
     );
